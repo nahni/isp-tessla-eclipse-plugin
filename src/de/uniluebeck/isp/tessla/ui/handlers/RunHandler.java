18d981f248bee444f6f62289b5c57c50ab789990
Annika Korz <annika.korz@student.uni-luebeck.de>
2017-04-29 19:48:21 +0200

    initial setup

---
+++ src/de/uniluebeck/isp/tessla/ui/handlers/RunHandler.java
@@ -0,0 +1,30 @@
+package de.uniluebeck.isp.tessla.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class RunHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Command command = event.getCommand();
+		command.getId();
+		
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Ui",
+				"Run");
+		return null;
+	}
+}

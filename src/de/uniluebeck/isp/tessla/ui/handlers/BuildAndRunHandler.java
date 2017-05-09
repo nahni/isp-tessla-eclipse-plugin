e0ecab787e7f907993015757736bf9fc5f552dc7
root <root@annika-VirtualBox>
2017-05-09 15:09:20 +0200

    Erstes Working Beispiel mit der Docker Client API

--- src/de/uniluebeck/isp/tessla/ui/handlers/BuildAndRunHandler.java
+++ src/de/uniluebeck/isp/tessla/ui/handlers/BuildAndRunHandler.java
@@ -1,11 +1,23 @@
 package de.uniluebeck.isp.tessla.ui.handlers;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.DockerCmdExecFactory;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
+
 import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
@@ -18,10 +30,51 @@ public class BuildAndRunHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Ui",
-				"Build And Run");
+//		MessageDialog.openInformation(
+//				window.getShell(),
+//				"Ui",
+//				"Build And Run");
+		
+		//Versuch 1
+//		try {
+////			Process p = Runtime.getRuntime().exec("docker images");
+//			Process p = Runtime.getRuntime().exec("docker load -i C:/Users/lenovo/Downloads/Atom/tessla");
+//			
+//			InputStream errors = p.getErrorStream();
+//			
+//			System.out.println("fertig");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			System.out.println("Exc");
+//		}
+		
+		DockerClientConfig  config  = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+		
+//		DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+//				  .withDockerHost("tcp://docker.somewhere.tld:2376")
+//				  .withDockerTlsVerify(true)
+//				  .withDockerCertPath("/home/user/.docker")
+//				  .withRegistryUsername(registryUser)
+//				  .withRegistryPassword(registryPass)
+//				  .withRegistryEmail(registryMail)
+//				  .withRegistryUrl(registryUrl)
+//				  .build();
+		
+		// using jaxrs/jersey implementation here (netty impl is also available)
+		DockerCmdExecFactory dockerCmdExecFactory = new JerseyDockerCmdExecFactory()
+		  .withReadTimeout(1000)
+		  .withConnectTimeout(1000)
+		  .withMaxTotalConnections(100)
+		  .withMaxPerRouteConnections(10);
+
+		DockerClient dockerClient = DockerClientBuilder.getInstance(config)
+		  .withDockerCmdExecFactory(dockerCmdExecFactory)
+		  .build();
+		
+		Info info = dockerClient.infoCmd().exec();
+		System.out.println("Info");
+		System.out.print(info);
+		
 		return null;
 	}
 }

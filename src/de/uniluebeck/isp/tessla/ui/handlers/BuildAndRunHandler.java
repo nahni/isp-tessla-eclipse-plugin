35b22ce0423e4be8fc8dfea4dc0ac1fc02dc82c4
root <root@annika-VirtualBox>
2017-05-16 16:58:40 +0200

    Versuch 1: Docker API tessla

--- src/de/uniluebeck/isp/tessla/ui/handlers/BuildAndRunHandler.java
+++ src/de/uniluebeck/isp/tessla/ui/handlers/BuildAndRunHandler.java
@@ -2,6 +2,10 @@ package de.uniluebeck.isp.tessla.ui.handlers;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
@@ -11,6 +15,7 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.DockerCmdExecFactory;
 import com.github.dockerjava.api.model.Info;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
@@ -74,6 +79,38 @@ public class BuildAndRunHandler extends AbstractHandler {
 		Info info = dockerClient.infoCmd().exec();
 		System.out.println("Info");
 		System.out.print(info);
+		
+		
+//		Create new Docker container, start and stop it:
+		//geht iwie nicht
+//		CreateContainerResponse container = dockerClient.createContainerCmd("file://home/annika/Entwicklung/Files/tessla")
+//				.withCmd("touch", "/test")
+//				.exec();
+		
+//		CreateContainerResponse container = dockerClient.createContainerCmd("busybox")
+//		.withCmd("touch", "/test")
+//		.exec();
+
+		//Alternative
+//		Path path = Paths.get(URI.create("file://home/annika/Entwicklung/Files/tessla"));
+//		Path path = Paths.get("file://home/annika/Entwicklung/Files/tessla");
+//		Path path = Paths.get("/home/annika/Entwicklung/Files/tessla");
+		Path path = Paths.get("home/annika/Entwicklung/Files/tessla");
+//		Path path = Paths.get(URI.create("/home/annika/Entwicklung/Files/tessla"));
+		//		dockerClient.execCreateCmd(arg0)
+		try {
+			InputStream is = Files.newInputStream(path, null);
+			dockerClient.loadImageCmd(is).exec();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		dockerClient.startContainerCmd(container.getId()).exec();
+//		dockerClient.stopContainerCmd(container.getId()).exec();
+//		dockerClient.waitContainerCmd(container.getId()).exec();
+
+		System.out.println("fertig");
 		
 		return null;
 	}

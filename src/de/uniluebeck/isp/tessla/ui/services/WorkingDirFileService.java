9132b23afa900c838f1d549fa6bcb92c3645c3f3
root <annikakorz@hotmail.com>
2017-06-13 15:57:00 +0200

    createZlogFile

--- src/de/uniluebeck/isp/tessla/ui/services/WorkingDirFileService.java
+++ src/de/uniluebeck/isp/tessla/ui/services/WorkingDirFileService.java
@@ -1,7 +1,9 @@
 package de.uniluebeck.isp.tessla.ui.services;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -60,11 +62,30 @@ public class WorkingDirFileService {
 //	    var formats = '[formats]\n'
 //	    formats += 'variable_values = "' + atom.config.get('tessla.variableValueFormatting') + '"\n'
 //	    formats += 'function_calls = "'  + atom.config.get('tessla.functionCallFormatting') + '"\n'
-//
+//	    
+		
+		try {
+			PrintWriter out = new PrintWriter(this.activeProject.getContainerDir() + "/" + "zlog.conf");
+			
+			String binName   = this.activeProject.getBinName();
+			
+			String atom_config_variableValueFormatting = "%m %d(%s) %us%n";
+			String atom_config_functionCallFormatting = "%m nil %d(%s) %us%n";
+			String formats = "[formats]\n";
+			formats = formats + "variable_values = \"" + atom_config_variableValueFormatting + "\"\n";
+			formats = formats + "function_calls = \""  + atom_config_functionCallFormatting + "\"\n";
+			
+			
+			
 //	    var rules = '[rules]\n'
 //	    rules += 'variable_values_cat.DEBUG "' + 'instrumented_' + binName + '.trace"; variable_values\n'
 //	    rules += 'function_calls_cat.DEBUG "'  + 'instrumented_' + binName + '.trace"; function_calls\n'
 //
+			String rules = "[rules]\n";
+			rules = rules + "variable_values_cat.DEBUG \"" + "instrumented_" + binName + ".trace\"; variable_values\n";
+			rules = rules + "function_calls_cat.DEBUG \""  + "instrumented_" + binName + ".trace\"; function_calls\n";
+			
+			
 //	    // first remove existing zlog
 //	    if ( fs.existsSync(path.join(this.containerDir, 'zlog.conf')) ) {
 //	      fs.unlinkSync(path.join(this.containerDir, 'zlog.conf'))
@@ -72,9 +93,19 @@ public class WorkingDirFileService {
 //
 //	    // then create new zlog.conf file
 //	    fs.writeFileSync(path.join(this.containerDir, 'zlog.conf'), formats + rules)
+			
+			out.println(formats);
+			out.println(rules);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
 	
 	
 }
+ 

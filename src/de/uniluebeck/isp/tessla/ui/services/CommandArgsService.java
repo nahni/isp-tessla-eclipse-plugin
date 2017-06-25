fc2f3b0053e0b61fd04340d5e25d776b2649c67e
root <annikakorz@hotmail.com>
2017-06-25 22:56:37 +0200

    Docker commands Version 2: changes from tessla plugin isp

---
+++ src/de/uniluebeck/isp/tessla/ui/services/CommandArgsService.java
@@ -0,0 +1,174 @@
+package de.uniluebeck.isp.tessla.ui.services;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import de.uniluebeck.isp.tessla.model.TeSSLaProject;
+import de.uniluebeck.isp.tessla.util.FileUtil;
+
+public class CommandArgsService {
+
+	TeSSLaProject activeProject;
+	
+	public CommandArgsService(TeSSLaProject activeProject){
+		this.activeProject = activeProject;
+	}
+	
+	public String[] getCompileToLLVM_BC_Args() {
+		List<String> args = new ArrayList<String>();
+		
+		String binName = activeProject.getBinName();
+//		String outFile = "build/" + binName + ".bc";
+		String outFile = binName + ".bc";
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && clang -ggdb -Wall -pedantic -c -emit-llvm main.c -o main.bc'
+//		clang -ggdb -Wall -pedantic -c -emit-llvm main.c -o main.bc'
+	
+		args.addAll(Arrays.asList("clang", "-ggdb", "-Wall", "-pedantic", "-c", "-emit-llvm"));
+		
+		List<File> cFiles = activeProject.getCFiles();
+		if(CollectionUtils.isNotEmpty(cFiles)){
+			for (File cFile : cFiles) {
+				String relativePath = FileUtil.getRelativePath(activeProject.getProjectPath(), cFile.getAbsolutePath());
+				args.add(relativePath);
+			}
+		}
+		
+		args.addAll(Arrays.asList("-o", outFile));
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getCompileToLLVM_BC_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+	}
+	
+	public String[] getInstrument_BC_Args() {
+		List<String> args = new ArrayList<String>();
+		
+		String binName = activeProject.getBinName();
+		String bc_file = binName + ".bc";
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && instrument main.bc'
+		
+		args.addAll(Arrays.asList("instrument", bc_file));
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getInstrument_BC_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+	}
+	
+	public String[] getCompileToBinary_Args() {
+		List<String> args = new ArrayList<String>();
+		
+		String binName = activeProject.getBinName();
+		String bc_file = binName + ".bc";
+		
+		String outFile = this.activeProject.getBinName();
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && clang -linstrumentation main.bc -o main'
+//		clang -linstrumentation main.bc -o main
+		
+		args.addAll(Arrays.asList("clang", "-linstrumentation", bc_file, "-o", outFile));
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getCompileToBinary_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+		
+	}
+	
+	public String[] getRunBinary_Args() {
+		List<String> args = new ArrayList<String>();
+		
+		String execFile = this.activeProject.getBinName();
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && ./main'
+		
+		
+		args.add("./" + execFile);
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getRunBinary_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+		
+	}
+	
+	public String[] getAddStandardLibrary_Args() {
+		List<String> args = new ArrayList<String>();
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && cat /usr/local/opt/tessla_rv/streams.tessla spec.tessla > spec2.tessla'
+//		cat /usr/local/opt/tessla_rv/streams.tessla spec.tessla > spec2.tessla'
+//		V1
+//		args.addAll(Arrays.asList("cat", "/usr/local/opt/tessla_rv/streams.tessla spec.tessla", ">", "spec2.tessla"));
+		
+//		Version 2:
+//		docker exec tessla cat spec.tessla /usr/local/opt/tessla_rv/streams.tessla > spec2.tessla
+		args.addAll(Arrays.asList("cat", "spec.tessla", "/usr/local/opt/tessla_rv/streams.tessla", ">", "spec2.tessla"));
+		
+//		args.addAll(Arrays.asList("cat", "'spec.tessla'", "'/usr/local/opt/tessla_rv/streams.tessla'", ">", "'spec2.tessla'"));
+		
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getAddStandardLibrary_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+	}
+	
+	public String[] getRunTeSSLa_Args() {
+		List<String> args = new ArrayList<String>();
+		
+//		docker run --volume /home/annika/Downloads/Docker2_Beispiel:/tessla --rm tessla sh -c 'cd /tessla && tessla spec2.tessla traces.log'
+//		tessla spec2.tessla traces.log'
+		
+		args.addAll(Arrays.asList("tessla", "spec2.tessla", "traces.log"));
+		
+        String command = "";
+        for (String string : args) {
+            command = command + " " + string;
+        }
+        System.out.println("getRunTeSSLa_Args: " + command);
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+
+		return argsArray;
+		
+		
+	}
+}
---
+++ src/de/uniluebeck/isp/tessla/ui/services/Main2.java
@@ -0,0 +1,114 @@
+package de.uniluebeck.isp.tessla.ui.services;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.spotify.docker.client.exceptions.DockerCertificateException;
+import com.spotify.docker.client.exceptions.DockerException;
+
+import de.uniluebeck.isp.tessla.model.TeSSLaProject;
+
+public class Main2 {
+
+	private final static String PROJECT_PATH = "/home/annika/Entwicklung/Spielwiese/dummyProjectPath3/sub_add_alternation";
+
+	private final static String OUTPUT_DIR = "";
+	private final static String BIN_NAME = "";
+	
+	TeSSLaProject activeProject;
+	
+	public static void main(String[] args) throws FileNotFoundException, DockerCertificateException, IOException, DockerException, InterruptedException {
+		Main2 main = new Main2();
+//		main.copyFiles();
+		main.run();
+	}
+	
+	public void run() throws FileNotFoundException, DockerCertificateException, IOException, DockerException, InterruptedException{
+		activeProject = new TeSSLaProject(PROJECT_PATH, OUTPUT_DIR, BIN_NAME);
+		
+		DockerService2 dockerSerivce = new DockerService2();
+//		dockerSerivce.startDocker(activeProject);
+		
+		Thread.sleep(3000);
+		
+		CommandArgsService commandArgsService = new CommandArgsService(activeProject);
+		
+		String[] compileToLLVM_BC_Args = commandArgsService.getCompileToLLVM_BC_Args();
+		dockerSerivce.runDockerCommand2(StringUtils.join(compileToLLVM_BC_Args, " "));
+		
+		Thread.sleep(3000);
+		
+		String[] instrument_BC_Args = commandArgsService.getInstrument_BC_Args();
+		dockerSerivce.runDockerCommand2(StringUtils.join(instrument_BC_Args, " "));
+		
+		Thread.sleep(3000);
+		
+		String[] compileToBinary_Args = commandArgsService.getCompileToBinary_Args();
+		dockerSerivce.runDockerCommand2(StringUtils.join(compileToBinary_Args, " "));
+		
+		Thread.sleep(3000);
+		
+		String[] runBinary_Args = commandArgsService.getRunBinary_Args();
+		dockerSerivce.runDockerCommand2(StringUtils.join(runBinary_Args, " "));
+		
+		Thread.sleep(3000);
+		
+		//das hier funktioniert nicht 
+		//cat: '>': No such file or directory
+//		cat: spec2.tessla: No such file or directory
+		//gleicher Befehl auf der Konsole per Hand funktioniert
+		String[] addStandardLibrary_Args = commandArgsService.getAddStandardLibrary_Args();
+//		dockerSerivce.runDockerCommand2(StringUtils.join(addStandardLibrary_Args, " "));
+//		dockerSerivce.runDockerCommand2(StringUtils.join(addStandardLibrary_Args, " "));
+//		dockerSerivce.runDockerCommand2("'cat spec.tessla /usr/local/opt/tessla_rv/streams.tessla > spec2.tessla'");
+//		dockerSerivce.runDockerCommand2("sh -c 'cd /tessla && cat spec.tessla /usr/local/opt/tessla_rv/streams.tessla > spec2.tessla'");
+//		dockerSerivce.runDockerCommandCat();
+//		dockerSerivce.runDockerCommandCat2(activeProject);
+		
+		List<String> args2 = new ArrayList<String>();
+		args2.addAll(Arrays.asList("docker", "exec", "tessla", "sh", "-c", "cat spec.tessla /usr/local/opt/tessla_rv/streams.tessla > spec2.tessla"));
+		
+		String[] argsArray2 = new String[args2.size()];
+		argsArray2 = args2.toArray(argsArray2);
+		dockerSerivce.runDockerCommandAvoidingWordSplitting(argsArray2);
+		
+		Thread.sleep(3000);
+
+		//das gleiche Problem: das geht mit docker exec nicht
+//		sudo docker run --volume /home/annika/.tessla-env:/tessla --rm tessla sh -c 'cd /tessla && tessla spec2.tessla traces.log'
+		String[] runTeSSLa_Args = commandArgsService.getRunTeSSLa_Args();
+//		dockerSerivce.runDockerCommand2(StringUtils.join(runTeSSLa_Args, " "));
+		
+		//ganz komisch. AUs eclipse raus geht das nicht. Fehler:
+		// /tessla: 1: /tessla: Syntax error: Unterminated quoted string
+		//aber wenn ich das 1:1 so ins terminal kopiere funktionierts
+//		dockerSerivce.runDockerCommandRunTessla(activeProject);
+		
+		// das hier duerfte funktionieren
+		//sudo docker exec tessla sh -c 'cd /tessla && tessla spec2.tessla traces.log'
+		List<String> args = new ArrayList<String>();
+		args.addAll(Arrays.asList("docker", "exec", "tessla", "sh", "-c", "cd /tessla && tessla spec2.tessla traces.log"));
+		
+		String[] argsArray = new String[args.size()];
+		argsArray = args.toArray(argsArray);
+		dockerSerivce.runDockerCommandAvoidingWordSplitting(argsArray);
+		
+		
+		
+	}
+	
+	public void copyFiles(){
+		activeProject = new TeSSLaProject(PROJECT_PATH, OUTPUT_DIR, BIN_NAME);
+		
+		//iwie sollte das wo anders hin oder die Methode umbeannt werden
+		WorkingDirFileService workingDirFileService = new WorkingDirFileService(activeProject);
+		workingDirFileService.createWorkingDir();
+		workingDirFileService.transferFilesToContainer();
+	}
+
+}
